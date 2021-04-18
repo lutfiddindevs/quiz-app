0186e39d5e2c50ef0b8b487f9ce58f23a85de71b
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-18 11:44:38 +0500

    crud method

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Question;
+use App\Models\Quiz;
 
 class Quiz extends Model
 {
@@ -19,4 +20,24 @@ class Quiz extends Model
     public function questions() {
     	return $this->hasMany(Question::class);
     }
+
+    public function storeQuiz($data) {
+    	return Quiz::create($data);
+    }
+
+    public function allQuiz() {
+        return Quiz::all();
+    }
+
+    public function getQuizById($id) {
+        return Quiz::find($id);
+    }
+
+    public function updateQuiz($data, $id) {
+        return Quiz::find($id)->update($data);
+    }
+
+    public function deleteQuiz($id) {
+    	return Quiz::find($id)->delete();
+    } 
 }
