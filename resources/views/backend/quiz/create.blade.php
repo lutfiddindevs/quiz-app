623324789e6f27727ff6e366ce4115cd3a98b41e
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-18 11:45:20 +0500

    create quiz page

---
+++ resources/views/backend/quiz/create.blade.php
@@ -0,0 +1,61 @@
+@extends('backend.layouts.master')
+
+@section('title','create quiz')
+
+@section('content')
+
+	<div class="span9">
+		<div class="content">
+			@if(Session::has('message'))
+            	<div class="alert alert-success">
+            		{{ Session::get('message') }}
+            	</div>
+			@endif
+			<form action="{{ route('quiz.store') }}" method="post">
+				@csrf
+			<div class="module">
+				<div class="module-head">
+					<h3>Create Quiz</h3>
+				</div>
+				<div class="module-body">
+					<div class="control-group">
+						<label class="control-label">Quiz Name</label>
+						<div class="controls">
+							<input type="text" name="name" class="span8" placeholder="Enter the name of a quiz" value="{{ old('name') }}"> <br>
+							@error('name')
+							    <span class="invalid-feedback" role="alert">
+							        <strong>{{ $message }}</strong>
+							    </span>
+							@enderror
+						</div>
+						<label class="control-label">Quiz Description</label>
+						<div class="controls">
+							<textarea name="description" class="span8" placeholder="Enter the description of a quiz">
+								{{ old('description') }}
+							</textarea> <br>
+							@error('description')
+							    <span class="invalid-feedback" role="alert">
+							        <strong>{{ $message }}</strong>
+							    </span>
+							@enderror
+						</div>
+						<label class="control-label">Quiz Duration</label>
+						<div class="controls">
+							<input type="text" name="minutes" class="span8" placeholder="Enter Quiz Duration" value="{{ old('minutes') }}"> <br>
+							@error('minutes')
+							    <span class="invalid-feedback" role="alert">
+							        <strong>{{ $message }}</strong>
+							    </span>
+							@enderror
+						</div>
+						<div class="controls">
+							<button type="submit" class="btn btn-success">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		  </form>
+		</div>
+	</div>
+	
+@endsection
