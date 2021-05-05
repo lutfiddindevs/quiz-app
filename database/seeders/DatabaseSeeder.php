30ec4ae63f58ef8b9ffeb0071d9c6f9b87a81950
Lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-05-05 23:31:23 +0500

    creating a seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $admin = new User();
+        $admin->name = 'admin';
+        $admin->email = 'admin123@gmail.com';
+        $admin->password = bcrypt('password');
+        $admin->visible_password = 'password';
+        $admin->email_verified_at = NOW();
+        $admin->occupation = 'CEO';
+        $admin->address = 'England';
+        $admin->phone = '4106677';
+        $admin->is_admin = 1;
+        $admin->save();
     }
 }
