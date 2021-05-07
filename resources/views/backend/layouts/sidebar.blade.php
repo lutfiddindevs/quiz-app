0f5822b08ab30105c38f35dd6aa15520babdcb10
Lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-05-07 05:57:31 +0500

    added links to sidebar

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -9,19 +9,24 @@
                         <li><a href="{{ route('quiz.create') }}"><i class="menu-icon icon-bullhorn"></i>Create Quiz </a>
                         </li>
                         <li><a href="{{ route('quiz.index') }}"><i class="menu-icon icon-inbox"></i>View Quiz <b class="label green pull-right">
-                            11</b> </a></li>
+                            </b> </a></li>
                     </ul>
                     <ul class="widget widget-menu unstyled">
                         <li><a href="{{ route('question.create') }}"><i class="menu-icon icon-bullhorn"></i>Create Question </a>
                         </li>
                         <li><a href="{{ route('question.index') }}"><i class="menu-icon icon-inbox"></i>View Question <b class="label green pull-right">
-                            17 new</b> </a></li>
+                            </b> </a></li>
                     </ul>
                     <ul class="widget widget-menu unstyled">
                         <li><a href="{{ route('user.create') }}"><i class="menu-icon icon-bullhorn"></i>Create User </a>
                         </li>
                         <li><a href="{{ route('user.index') }}"><i class="menu-icon icon-inbox"></i>View User<b class="label green pull-right">
-                            17 new</b> </a></li>
+                            </b> </a></li>
+                    </ul>
+                    <ul class="widget widget-menu unstyled">
+                        <li><a href="{{ route('assign.exam') }}"><i class="menu-icon icon-bullhorn"></i>Assign Quiz </a>
+                        </li>
+                        <li><a href="{{ route('exam.view') }}"><i class="menu-icon icon-inbox"></i>View User Quiz<b class="label green pull-right"></b> </a></li>
                     </ul>
                     <!--/.widget-nav-->
                     
